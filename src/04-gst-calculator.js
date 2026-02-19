0efb7a5237489cbd26da03dfc1ff3b3993ee9594
Babita Kumari <babitakumari.ky3007@gmail.com>
2026-02-19 19:37:19 +0530

    gst calculator solved

--- src/04-gst-calculator.js
+++ src/04-gst-calculator.js
@@ -39,5 +39,25 @@
  *   // => { baseAmount: 500, gstRate: 0, gstAmount: 0, totalAmount: 500 }
  */
 export function calculateGST(amount, category) {
-  // Your code here
+    if(!Number.isFinite(amount) || amount <= 0 || typeof category !== "string")
+      return null;
+
+    const gstRates = {
+      essential: 0,
+      food: 5,
+      standard: 12,
+      electronics: 18,
+      luxury: 28
+    };
+
+    const categoryLower = category.toLowerCase(); 
+
+    if(!(categoryLower in gstRates))
+        return null;
+
+    const gstRate = gstRates[categoryLower];
+    const gstAmount = parseFloat(((amount * gstRate)/100).toFixed(2));
+    const totalAmount =parseFloat((amount + gstAmount).toFixed(2));
+
+    return { baseAmount: amount, gstRate: gstRate, gstAmount: gstAmount, totalAmount: totalAmount};
 }
